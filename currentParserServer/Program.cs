49479595deb6a40fb3c3d69f49147769548dae72
MrLlNK <Mbrandt234@gmail.com>
2023-11-19 15:19:29 +0100

    Add Server.cs file and replace Server class to this file

--- currentParserServer/Program.cs
+++ currentParserServer/Program.cs
@@ -4,38 +4,10 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 
-namespace currentParser.Server
+namespace currentParserServer
 {
 
-    public class Server
-    {
-        public const int port = 10000;
-        public IPAddress ipAddress = IPAddress.Any;
-        public const int sleepTimer = 200;
+ 
 
-        public Server() { 
-        
-            TcpListener tcpListener = new TcpListener(ipAddress, port);
-            Debug.WriteLine("Listen on port: " + port.ToString());
-            try
-            {
-                tcpListener.Start();
-                while (!tcpListener.Pending())
-                {
-                    Thread.Sleep(sleepTimer);
-                }
-
-                Socket socket = tcpListener.AcceptSocket();
-
-            }
-            catch (Exception err)
-            {
-                throw new Exception("Fehler beim Verbindungserkennung", err);
-            }      
-        }
-
-
-
-    }
 
 }
